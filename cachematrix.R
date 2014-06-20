37fa06105220b42258dc75fdf7e46c5baf1fc3c3
akka2001hk <akka2001hk@yahoo.com>
2014-06-20 21:49:51 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+y <- matrix() ## initialize local variables y,m
+	  m <- NULL
+        set <- function(y) {  ## store the new value 
+                x <<- y
+                m <<- NULL
+        }
+        getmatrix <- function() x
+        invertmatrix <- function(solve) m <<- solve
+        getinvert <- function() m
+        list(set = set, getmatrix = getmatrix,
+             invertmatrix = invertmatrix,
+             getinvert = getinvert)
 
 }
 
@@ -11,5 +23,16 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        data <- matrix()   ## initialize variable
+        m <- x$getinvert() ## get invert matrix if calculated already
+ 	  if(!is.null(m) && identical(x$getmatrix(),x)) {    ## inverse has already been calculated (and the matrix has not changed
+                message("getting cached data")
+                return(m) ## return inverse calculated previously
+        }
+	  data <- x$getmatrix() ## get the original matrix
+	  if(det(data) == 0) stop ("Matrix is singular and cannot be inverted") ## check if it can be inverted
+	  m <- solve(data) ## compute the inverse
+        m ## return result
+
+## Return a matrix that is the inverse of 'x'
 }
